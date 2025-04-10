4112fdc4300be6ac1769340e8c7f23f2063e24b9
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-04-10 09:40:52 -0300

    Update and rename mips_em_C.c to mips_em_C_1004.c

--- mips_em_C_1004.c
+++ mips_em_C_1004.c
@@ -6,7 +6,7 @@
 	1011	 => LW
 	1111	 => SW
 	1000	 => BEQ
-	0010	 => J		*/
+	0010	 => J	     */
 
     #include <stdio.h>
     #include <stdlib.h>
@@ -47,19 +47,21 @@
     } Deco;
     
     //NOMES DAS FUNCOES
+    void menu();
+    void menuOP();
     int contarlinhas(const char *arquivo);
+    void carregaMemInst(char mem[256][17]);
+    void carregarMemoriaDados(int mem[256]);
+    void printMemory(char mem[256][17], struct instrucao *inst, Deco *dec);
+    void printmemory(int *memdado);
+    void printReg(int *reg);
+    void decodificarInstrucao(const char *bin, struct instrucao *inst, Deco *dec);
+    void copiarBits(const char *instrucao, char *destino, int inicio, int tamanho);
     int binarioParaDecimal(const char *bin);
     int extensaobits(int valor, int bits);
-    void carregaMemInst(char mem[256][17]);
-    void printMemory(char mem[256][17], struct instrucao *inst, Deco *dec);
-    void printReg(int *reg);
-    void menu();
-    void progULA(Deco *inst, int *reg, int *memdado, int *pc);
-    void copiarBits(const char *instrucao, char *destino, int inicio, int tamanho);
-    void decodificarInstrucao(const char *bin, struct instrucao *inst, Deco *dec);
     void printInstrucao(Deco *dec);
-    void printmemory(int *memdado);
-    void carregarMemoriaDados(int mem[256]);
+    void controle(Deco *inst, int *reg, int *memdado, int *pc);
+    int ULA(int op1, int op2, int opULA);
     
     //PROGRAMA PRINCIPAL
     int main() 
@@ -80,18 +82,7 @@
         int op, nlinhas, resul, pc = 0;
         
         do {
-            printf("\n\n *** MENU *** \n");
-            printf("1 - Carregar memoria de instrucoes\n");
-            printf("2 - Carregar memoria de dados\n");
-            printf("3 - Imprimir memorias\n");
-            printf("4 - Imprimir banco de registradores\n");
-            printf("5 - Imprimir todo o simulador\n");
-            printf("6 - Salvar .asm\n");
-            printf("7 - Salvar .dat\n");
-            printf("8 - Executar programa\n");
-            printf("9 - Executar instrucao\n");
-            printf("10 - Volta uma instrucao\n");
-            printf("11 - Sair\n\n");
+            menuOP();
             printf("Sua escolha: ");
             scanf("%d", &op);
     
@@ -120,7 +111,9 @@
                 break;
     
             case 5:
-                printf("Em desenvolvimento.");
+                printMemory(meminst, &instrucao, &dec);
+                printmemory(memdados);
+		printReg(registrador);
                 break;
     
             case 6:
@@ -135,8 +128,6 @@
                 printf("Em desenvolvimento.");
     
             case 9:
-                printf("Em desenvolvimento.\n\n");
-                
                 decodificarInstrucao(meminst[pc], &instrucao, &dec);
                 int pc_antes = pc;
                 printInstrucao(&dec);
@@ -160,6 +151,21 @@
     }
     
     //FUNCOES IMPLEMENTADAS
+    void menuOP() {
+	printf("\n\n *** MENU *** \n");
+        printf("1 - Carregar memoria de instrucoes\n");
+        printf("2 - Carregar memoria de dados\n");
+        printf("3 - Imprimir memorias\n")
+        printf("4 - Imprimir banco de registradores\n");
+        printf("5 - Imprimir todo o simulador\n");
+        printf("6 - Salvar .asm\n");
+        printf("7 - Salvar .dat\n");
+        printf("8 - Executar programa\n");
+        printf("9 - Executar instrucao\n");
+        printf("10 - Volta uma instrucao\n");
+        printf("11 - Sair\n\n");
+    }   
+
     int contarlinhas(const char *arquivo)
     {
         char c;
@@ -212,6 +218,23 @@
         fclose(arq);
     }
     
+void carregarMemoriaDados(int mem[256])	{
+    char arquivo[20];
+    printf("Nome do arquivo: ");
+    scanf("%s", arquivo);
+    FILE *arq = fopen(arquivo, "r");
+    if (!arq)
+    {
+    perror ("Erro ao abrir arquivo") ;
+    exit (1) ;
+    }
+    int i = 0;
+    while (fscanf(arq, "%d", &mem[i]) != EOF)
+    {
+        i++;
+    }
+}
+
     void printMemory(char mem[256][17], struct instrucao *inst, Deco *dec)
     {
         printf("\n############## MEMÓRIA DE INSTRUÇÕES ##############\n");
@@ -243,58 +266,7 @@
             printf("REGISTRADOR [%d] - %d\n", i, reg[i]);
         }
     }
-    
-    void progULA(Deco *dec, int *reg, int *memdado, int *pc)
-    {
-        if (dec->opcode == 11)
-        {
-            reg[dec->rt] = dec->imm;
-        }
-        else if (dec->opcode == 15)
-        {
-            reg[dec->rt] = reg[dec->rs] + memdado[dec->imm];
-        }
-        else if (dec->opcode == 11)
-        {
-            memdado[dec->rs + dec->imm] = reg[dec->rt];
-        }  
-        else if (dec->opcode == 4)
-        {
-            reg[dec->rt] = reg[dec->rs] + dec->imm;
-        }
-        else if (dec->opcode == 0 && dec->funct == 0)
-        {
-            reg[dec->rd] = reg[dec->rs] + reg[dec->rt];
-        }
-        else if (dec->opcode == 0 && dec->funct == 2)
-        {
-            reg[dec->rd] = reg[dec->rs] - reg[dec->rt];
-        }
-        else if (dec->opcode == 0 && dec->funct == 4)
-        {
-            reg[dec->rd] = reg[dec->rs] & reg[dec->rt];
-        }
-         else if (dec->opcode == 0 && dec->funct == 5)
-        {
-            reg[dec->rd] = reg[dec->rs] | reg[dec->rt];
-        }
-        else if (dec->opcode == 8) {
-            if (reg[dec->rs] == reg[dec->rt]) {
-                *pc = *pc + dec->imm;
-            }
-        }
-        else if (dec->opcode == 2){
-            *pc = dec->addr;
-        }
-    }
-    
-    // copia os bits da instrução para cada campo da struct instrucao
-    void copiarBits(const char *instrucao, char *destino, int inicio, int tamanho)
-    {
-        strncpy(destino, instrucao + inicio, tamanho); // Copia os bits desejados
-        destino[tamanho] = '\0';  // Adiciona o terminador de string
-    }
-    
+
     // decodifica a instrucao e armazena os valores na struct do tipo Deco ja no formato int
     void decodificarInstrucao(const char *bin, struct instrucao *inst, Deco *dec)
     {
@@ -315,17 +287,12 @@
       dec->addr = binarioParaDecimal(inst->addr);
       dec->addr = extensaobits(dec->addr, 7);
     }
-    
-    // Função para imprimir a instrução
-    void printInstrucao(Deco *dec)
+
+    // copia os bits da instrução para cada campo da struct instrucao
+    void copiarBits(const char *instrucao, char *destino, int inicio, int tamanho)
     {
-        printf("opcode: %d  ", dec->opcode);
-        printf("rs: %d  ", dec->rs);
-        printf("rt: %d  ", dec->rt);
-        printf("rd: %d  ", dec->rd);
-        printf("funct: %d  ", dec->funct);
-        printf("imm: %d  ", dec->imm);
-        printf("addr: %d", dec->addr);
+        strncpy(destino, instrucao + inicio, tamanho); // Copia os bits desejados
+        destino[tamanho] = '\0';  // Adiciona o terminador de string
     }
     
     // converte de binario para decimal
@@ -341,7 +308,7 @@
         
         return valor;
     }
-    
+
     // faz a extensão de 6 bits para 8 bits
     int extensaobits(int valor, int bits){
        
@@ -359,20 +326,73 @@
         }
     }
 
-void carregarMemoriaDados(int mem[256])
-{
-    char arquivo[20];
-    printf("Nome do arquivo: ");
-    scanf("%s", arquivo);
-    FILE *arq = fopen(arquivo, "r");
-    if (!arq)
+    // Função para imprimir a instrução
+    void printInstrucao(Deco *dec)
     {
-    perror ("Erro ao abrir arquivo") ;
-    exit (1) ;
+        printf("opcode: %d  ", dec->opcode);
+        printf("rs: %d  ", dec->rs);
+        printf("rt: %d  ", dec->rt);
+        printf("rd: %d  ", dec->rd);
+        printf("funct: %d  ", dec->funct);
+        printf("imm: %d  ", dec->imm);
+        printf("addr: %d", dec->addr);
     }
-    int i = 0;
-    while (fscanf(arq, "%d", &mem[i]) != EOF)
+
+    void controle(Deco *dec, int *reg, int *memdado, int *pc)
     {
-        i++;
+        if (dec->opcode == 11)
+        {
+            reg[dec->rt] = dec->imm;
+        }
+        else if (dec->opcode == 15)
+        {
+            reg[dec->rt] = ULA(reg[dec->rs], memdado[dec->imm], 0);
+        }
+        else if (dec->opcode == 11)
+        {
+            memdado[ULA(dec->rs, dec->imm, 0)] = reg[dec->rt];
+        }  
+        else if (dec->opcode == 4)
+        {
+            reg[dec->rt] = ULA(reg[dec->rs], dec->imm, 0);
+        }
+        else if (dec->opcode == 0 && dec->funct == 0)
+        {
+            reg[dec->rd] = ULA(reg[dec->rs], reg[dec->rt], 0);
+        }
+        else if (dec->opcode == 0 && dec->funct == 2)
+        {
+            reg[dec->rd] = ULA(reg[dec->rs], reg[dec->rt], 2);
+        }
+        else if (dec->opcode == 0 && dec->funct == 4)
+        {
+            reg[dec->rd] = ULA(reg[dec->rs], reg[dec->rt], 4);
+        }
+         else if (dec->opcode == 0 && dec->funct == 5)
+        {
+            reg[dec->rd] = ULA(reg[dec->rs], reg[dec->rt], 5);
+        }
+        else if (dec->opcode == 8) {
+            if (reg[dec->rs] == reg[dec->rt]) {
+                *pc = ULA(*pc, dec->imm, 0);
+            }
+        }
+        else if (dec->opcode == 2){
+            *pc = dec->addr;
+        }
     }
-}
+
+    int ULA(int op1, int op2, int opULA) {
+        if(opULA == 0) {
+            return op1 + op2;
+        }
+        else if(opULA == 2) {
+            return op1 - op2;
+        }
+        else if(opULA == 4) {
+            return op1 & op2;
+        }
+        else if(opULA == 5) {
+            return op1 | op2;
+        }
+    }
